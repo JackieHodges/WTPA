b8d0e0897ac67893101d348de1cbff7078f09132
Jackie Hodges <jackie.hodges2015@gmail.com>
2021-10-19 14:42:59 -0500

    heroku deployment

--- server.js
+++ server.js
@@ -8,8 +8,12 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 
 app.use(routes);
