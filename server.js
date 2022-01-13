11f069d6d6f16f276f9ffe0b50d22932d3dbc83d
Jackie Hodges <jackie.hodges2015@gmail.com>
2022-01-13 13:45:39 -0600

    further changes to the server file for deployment bugs

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const sequelize = require('./config/connection');
 const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
-const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,10 +11,6 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 }
-
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
 
 app.use(routes);
 
