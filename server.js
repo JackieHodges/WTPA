202b59fb1d99e885affa570f2df66e9a37614fb1
Jackie Hodges <jackie.hodges2015@gmail.com>
2022-01-13 13:39:39 -0600

    slight change to server file to debug route issues

--- server.js
+++ server.js
@@ -4,17 +4,18 @@ const sequelize = require('./config/connection');
 const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
 }
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 app.use(routes);
 
