40f44cccb7e67a8706f3deffa877449ffb212b73
Jackie Hodges <jackie.hodges2015@gmail.com>
2023-05-17 17:43:18 -0500

    updated logic for sql connection

--- config/connection.js
+++ config/connection.js
@@ -3,15 +3,24 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
+if (process.env.DB_USER === "root") {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
       host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    },
+  );
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER_AWS,
+    process.env.DB_PASSWORD_AWS,
+    {
+      host: 'wtpa-database.cluster-cyk8et2xxhke.us-east-2.rds.amazonaws.com',
       dialect: 'mysql',
       port: 3306,
     },
